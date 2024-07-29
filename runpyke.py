ec6f1d58d0513e7a7a3ce93ef22a3d745b5e4e5f
Jonathan Hahn <helo9@users.noreply.github.com>
2024-07-29 14:49:17 +0200

    Change almost everything

--- runpyke.py
+++ runpyke.py
@@ -5,6 +5,38 @@ import subprocess
 from pathlib import Path
 from warnings import warn
 
+
+def main():
+	interpreters = collect_kernel_interpreters()
+
+	parser = argparse.ArgumentParser(
+		"runpyke",
+		description="direkt kernel program-runner",
+	)
+
+	parser.add_argument(
+		"kernel",
+		type=str
+	)
+
+	parser.add_argument(
+		"interpreter_argument",
+		nargs="*"
+	)
+
+	args, pip_args = parser.parse_known_args()
+
+	if args.kernel in interpreters:
+
+		interpreter_path = interpreters[args.kernel]
+
+		run(interpreter_path, pip_args)
+
+	else:
+		kernellist = ", ".join(interpreters.keys())
+		raise RuntimeWarning(f"Kernel {args.kernel} was not found. There are\n{kernellist}")
+
+
 def collect_kernel_interpreters():
 
 	try:
@@ -35,47 +67,8 @@ def collect_kernel_interpreters():
 	return interpreters
 
 
-def locate_pip(interpreter_path):
-	python_binary_path = Path(interpreter_path).parent
-
-	pip_path = (python_binary_path /"pip")
-
-	if pip_path.exists == False:
-		raise FileNotFoundError("Could not find pip in interpreter folder")
-
-	return pip_path
-
-
-def run_pip(path: str, arguments: list):
-	subprocess.run([path] + arguments)
-
-
-def main():
-	interpreters = collect_kernel_interpreters()
-
-	parser = argparse.ArgumentParser(
-		"dkp",
-		description="direkt kernel program-runner",
-	)
-
-	parser.add_argument(
-		"kernel",
-		type=str
-	)
-
-	args, pip_args = parser.parse_known_args()
-
-	if args.kernel in interpreters:
-
-		interpreter_path = interpreters[args.kernel]
-
-		pip_path = locate_pip(interpreter_path)
-
-		run_pip(pip_path, pip_args)
-
-	else:
-		kernellist = ", ".join(interpreters.keys())
-		raise RuntimeWarning(f"Kernel {args.kernel} was not found. There are\n{kernellist}")
+def run(path: str, arguments: list):
+	return subprocess.run([path] + arguments)
 
 
 if __name__=="__main__":
